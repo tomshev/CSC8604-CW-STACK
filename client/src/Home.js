3c3b326ed8398e7827e96f9887a82f11a56d88f7
tomshev <shevcov7@icloud.com>
2024-03-13 18:56:20 +0000

    Adding style to Home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 function Home() {
 
     return (
-      <div>
-        <h1>Food Preference Analysing System</h1>
-        <Link to="/app">TEST FETCHING FROM SERVER</Link>
-        <br />
-        <Link to="/cashierview">CashierView PANEL</Link>
-        <br />
-        <Link to="/publicview">PublicView PANEL</Link>
-
+      <div className="homeContainer">
+            <h1>FOOD PREFERENCE ANALYSING SYSTEM</h1>
+            <div className="buttonContainer">
+            <Link to="/app">TEST FETCHING FROM SERVER</Link>
+            <br />
+            <Link to="/cashierview">CashierView PANEL</Link>
+            <br />
+            <Link to="/publicview">PublicView PANEL</Link>
+        </div>
       </div>
     );
   }
