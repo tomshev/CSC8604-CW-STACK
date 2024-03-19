1203d9e1c7d13ed3aa240c61aacd0725ba7bb1ef
tomshev <shevcov7@icloud.com>
2024-03-19 17:24:12 +0000

    Added structs for storing meal data

--- server/server.js
+++ server/server.js
@@ -8,16 +8,28 @@ app.use(express.json());
 
 startReadingCard();
 
+const mealCounts = {
+    "201": { Meal1: 0, Meal2: 0, Meal3: 0},
+    "203": { Meal1: 0, Meal2: 0, Meal3: 0}
+};
+
 // app.use(express.static("content"));
 
 app.post("/submitMeal", (req, res) => {
-    const meal = req.body;
+    const { meal } = req.body;
     console.log(meal);
     const sClass = getLastReadData();
     console.log(`Meal selected: ${JSON.stringify(meal)}, Card: ${sClass}`);
-
     // LOGIC FOR PROCESSING
+
     res.status(200).json({message: "Meal received"});
+
+    if (mealCounts[sClass] && mealCounts[sClass][meal] !== undefined) {
+        mealCounts[sClass][meal] += 1;
+        console.log(`Updated meal count for ${sClass}:`, mealCounts[sClass]);
+    } else {
+        res.status(400).json({ error: "Invalid class or meal" });
+    }
 })
 
 app.get("/api", (req,res) => {
