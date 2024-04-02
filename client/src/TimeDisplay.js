98c3baeb36859af5da9c5bee1d49b3a8df3558c9
tomshev <shevcov7@icloud.com>
2024-04-02 14:04:38 +0300

    Deleted files, updated docs

--- client/src/TimeDisplay.js
+++ client/src/TimeDisplay.js
@@ -1,6 +1,18 @@
+
+/*
+
+  FILE: TimeDisplay.js
+
+  CREATED BY: TOM SEVCOV (190379894)
+    
+  DESCRIPTION: This is a React component for displaying the current date and time as an element.
+
+*/
+
 import React, { useState, useEffect } from 'react';
 
 function TimeDisplay() {
+  
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
